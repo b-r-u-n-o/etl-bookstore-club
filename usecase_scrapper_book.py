417a49c3de09b7aefbd2ede9bac82cafd8807898
Bruno Soares <tsoares.bruno@gmail.com>
2023-09-10 21:54:27 -0300

    Insere estrtura de métodos para o use case scrapper book

--- usecase_scrapper_book.py
+++ usecase_scrapper_book.py
@@ -4,8 +4,21 @@ from entities.bookstore import Book
 from entities.scrapper import Scrapper
 
 # TODO: Definir métodos para executar as operações necessárias para obter os
-    # dados de livros
+# dados de livros
+
 
 class UseCaseBookScrapper:
-    
-    pass
+    def get_book_name(self):
+        pass
+
+    def get_book_category(self):
+        pass
+
+    def get_book_stars(self):
+        pass
+
+    def get_book_price(self):
+        pass
+
+    def get_book_status(self):
+        pass
